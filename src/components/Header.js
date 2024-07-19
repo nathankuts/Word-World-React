ece3c2304d575e026d17b314d54784a9c8616bab
nkutete <nathankutete99@gmail.com>
2024-07-19 18:37:49 +0300

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,1 +1,20 @@
+import React from 'react';
+import './Header.css';
 
+const Header = () => (
+    <header>
+        <div className="container">
+            <a href="index.html">
+                <h1 className="logo">Word World</h1>
+                <h5>Home for original stories</h5>
+            </a>
+            <nav>
+                <a href="/">HOME</a>
+                <a href="/light-novels" id="lightNovelsLink">LIGHT NOVELS</a>
+                <a href="/comics" id="comicsLink">COMICS</a>
+            </nav>
+        </div>
+    </header>
+);
+
+export default Header;
