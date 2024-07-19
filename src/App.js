ad24ab7d1c43975526b1e87056d720cc04fa1dab
nkutete <nathankutete99@gmail.com>
2024-07-19 18:41:26 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,1 +1,27 @@
+import React from 'react';
+import './App.css';
+import Header from './components/Header';
+import NoticeBoard from './components/NoticeBoard';
+import SignupForm from './components/SignupForm';
+import Teasers from './components/Teasers';
+import Footer from './components/Footer';
+
+function App() {
+    return (
+        <div className="App">
+            <Header />
+            <section className="container">
+                <NoticeBoard />
+                <div className="signup-container">
+                    <button className="signup-button" id="signupButton">Sign Up</button>
+                </div>
+            </section>
+            <SignupForm />
+            <Teasers />
+            <Footer />
+        </div>
+    );
+}
+
+export default App;
 
