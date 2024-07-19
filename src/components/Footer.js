3f3c8ed8137006026854e4ff9f505fea58619e32
nkutete <nathankutete99@gmail.com>
2024-07-19 18:39:00 +0300

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,1 +1,17 @@
+import React from 'react';
+import './Footer.css';
 
+const Footer = () => (
+    <footer>
+        <div className="container">
+            <small>&copy; Word World</small>
+            <nav>
+                <a href="/">HOME</a>
+                <a href="/light-novels">LIGHT NOVELS</a>
+                <a href="/comics">COMICS</a>
+            </nav>
+        </div>
+    </footer>
+);
+
+export default Footer;
