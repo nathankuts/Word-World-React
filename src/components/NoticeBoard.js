9eadc0a7f4f0df0261c9d5d1d77ba483087e181e
nkutete <nathankutete99@gmail.com>
2024-07-19 18:38:23 +0300

    Update NoticeBoard.js

--- src/components/NoticeBoard.js
+++ src/components/NoticeBoard.js
@@ -1,1 +1,40 @@
+import React, { useState, useEffect } from 'react';
+import './NoticeBoard.css';
+
+const NoticeBoard = () => {
+    const [newWorks, setNewWorks] = useState([
+        { title: 'The Adventures of Tom', link: 'published-chapter.html' },
+        { title: 'Mystery in the Woods', link: 'comics.html' },
+        { title: 'Love in the Time of Dragons', link: 'published-chapter.html' }
+    ]);
+
+    const loadNotices = () => {
+        // Implementation of loadNotices function
+    };
+
+    const fetchNewData = () => {
+        // Implementation of fetchNewData function
+    };
+
+    useEffect(() => {
+        loadNotices();
+        const interval = setInterval(fetchNewData, 10000);
+        return () => clearInterval(interval);
+    }, []);
+
+    return (
+        <div className="notice-board">
+            <h2>Newly Published Works</h2>
+            <ul id="noticeList">
+                {newWorks.map((work, index) => (
+                    <li key={index}>
+                        <a href={work.link}>{work.title}</a>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+};
+
+export default NoticeBoard;
 
