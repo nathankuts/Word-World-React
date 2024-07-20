d2ceeb32aa9c6b256862879070784ad46ad4e025
nkutete <nathankutete99@gmail.com>
2024-07-20 14:48:06 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css'; // Include the main CSS file if you have any
+
+ReactDOM.render(<App />, document.getElementById('root'));
