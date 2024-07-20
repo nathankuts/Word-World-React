168043a1dba6248275599d4cc152418cae885e38
nkutete <nathankutete99@gmail.com>
2024-07-20 14:19:09 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header';
 import NoticeBoard from './components/NoticeBoard';
@@ -6,24 +7,33 @@ import SignupForm from './components/SignupForm';
 import SignInForm from './components/SignInForm';
 import Teasers from './components/Teasers';
 import Footer from './components/Footer';
+import PublishedChapter from './components/PublishedChapter/PublishedChapter';
 
 function App() {
     return (
-        <div className="App">
-            <Header />
-            <section className="container">
-                <NoticeBoard />
-                <div className="signup-container">
-                    <button className="signup-button" id="signupButton">Sign Up</button>
-                </div>
-            </section>
-            <SignupForm />
-            <SignInForm />
-            <Teasers />
-            <Footer />
-        </div>
+        <Router>
+            <div className="App">
+                <Header />
+                <Switch>
+                    <Route exact path="/">
+                        <section className="container">
+                            <NoticeBoard />
+                            <div className="signup-container">
+                                <button className="signup-button" id="signupButton">Sign Up</button>
+                            </div>
+                        </section>
+                        <SignupForm />
+                        <SignInForm />
+                        <Teasers />
+                    </Route>
+                    <Route path="/chapter">
+                        <PublishedChapter />
+                    </Route>
+                </Switch>
+                <Footer />
+            </div>
+        </Router>
     );
 }
 
 export default App;
-
