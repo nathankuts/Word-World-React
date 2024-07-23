ef216f1a0cb9dfa3aa4c320b2aca39aba9589a98
nkutete <nathankutete99@gmail.com>
2024-07-23 12:56:34 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,41 +7,41 @@ import SignupForm from './components/SignupForm';
 import SignInForm from './components/SignInForm';
 import Teasers from './components/Teasers';
 import Footer from './components/Footer';
-import PublishedChapter from './components/PublishedChapter/PublishedChapter';
+import PublishedChapter from './components/PublishedChapter';
 import PublishForm from './components/PublishForm';
-import Comics from './components/Comics'; // Import the Comics component
+import Comics from './components/Comics';
 
 function App() {
-    return (
-        <Router>
-            <div className="App">
-                <Header />
-                <Switch>
-                    <Route exact path="/">
-                        <section className="container">
-                            <NoticeBoard />
-                            <div className="signup-container">
-                                <button className="signup-button" id="signupButton">Sign Up</button>
-                            </div>
-                        </section>
-                        <SignupForm />
-                        <SignInForm />
-                        <Teasers />
-                    </Route>
-                    <Route path="/chapter">
-                        <PublishedChapter />
-                    </Route>
-                    <Route path="/publish">
-                        <PublishForm />
-                    </Route>
-                    <Route path="/comics">
-                        <Comics /> {/* Add the Comics route */}
-                    </Route>
-                </Switch>
-                <Footer />
-            </div>
-        </Router>
-    );
+  return (
+    <Router>
+      <div className="App">
+        <Header />
+        <Switch>
+          <Route exact path="/">
+            <section className="container">
+              <NoticeBoard />
+              <div className="signup-container">
+                <button className="signup-button" id="signupButton">Sign Up</button>
+              </div>
+            </section>
+            <SignupForm />
+            <SignInForm />
+            <Teasers />
+          </Route>
+          <Route path="/chapter">
+            <PublishedChapter />
+          </Route>
+          <Route path="/publish">
+            <PublishForm />
+          </Route>
+          <Route path="/comics">
+            <Comics />
+          </Route>
+        </Switch>
+        <Footer />
+      </div>
+    </Router>
+  );
 }
 
 export default App;
